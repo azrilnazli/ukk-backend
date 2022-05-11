9dbf271f06a2ac9f58242a3eafa83bcb63fff753
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-11 22:44:05 +0800

    dashboard

--- resources/views/companies/all.blade.php
+++ resources/views/companies/all.blade.php
@@ -61,8 +61,8 @@
                         <span class="badge badge-warning">Resubmission</span> 
                         @endif
 
-                        @if($row->is_submitted == 1  && $row->is_rejected == 0 && $row->is_approved == 0 ) 
-                          <span class="badge badge-info">Pending</span
+                        @if($row->is_rejected == 0 && $row->is_approved == 0 ) 
+                          <span class="badge badge-info">Pending</span>
                         @endif
                 
                     </td>
