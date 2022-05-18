d4b71e870a419494273c954126d93e38f2798e4b
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-18 17:05:15 +0800

    VIDEO - failed video error missing relationship id

--- app/Services/VideoService.php
+++ app/Services/VideoService.php
@@ -43,6 +43,7 @@ class VideoService {
     public function failed($item = null)
     {
         return Video::query()
+            ->with(['user'])
             ->where('duration','=', 0)
             ->orderBy('id','desc')
             ->paginate($item)
