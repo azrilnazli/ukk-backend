7dc885e193684cb8be9c26132573d2c154ad2704
Azril Nazli Alias <sales@stream.my>
2022-05-19 02:55:46 +0800

    PROPOSAL - change icon

--- resources/views/tender_submissions/index.blade.php
+++ resources/views/tender_submissions/index.blade.php
@@ -61,10 +61,10 @@
                     <td> @if(isset($row->user->company))<span class="badge badge-warning">{{ $row->user->company->id }}</span> {{ $row->user->company->name }}@endif</td>
                     <td>{{ $row->tender->type }} - {{ $row->tender->tender_category }}</td>
                     <td>{{ $row->tender->programme_code }}</td>
-                    <td>{!! $row->is_pdf_cert_uploaded ? '<span class="text-success"><i class="fas fa-check"></i></span>' : '<span class="text-danger"><i class="fas fa-times"></span></i>' !!} </td>     
+                    <td>{!! $row->is_pdf_cert_uploaded ? '<span class="text-primary"><i class="fas fa-file-pdf"></i>' : '' !!} </td>     
                     <td>
                       @if(isset($row->video))
-                      {!! $row->video->is_ready ? '<span class="text-success"><i class="fas fa-check"></i></span>' : '<span class="text-danger"><i class="fas fa-times"></span></i>' !!} </td>     
+                      {!! $row->video->is_ready ? '<span class="text-red"><i class="fas fa-video"></i></span>' : '' !!} </td>     
                       @endif
                     <td class="text-center">
                       <a class="btn btn-success btn-sm" href="{{ route('tender_submissions.show', $row->id) }}">
