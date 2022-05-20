fabf13496cca443af083cba2f46bb679c121e703
Azril Nazli Alias <sales@stream.my>
2022-05-20 19:55:34 +0800

    betulkan logik check is_approved

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -13,92 +13,97 @@
 @stop
 
 @section('content')
-<form method="POST" action="{{ route('users.store') }}">
-@csrf
+
 <!-- Horizontal Form -->
-<div class="card card-dark">
-    <div class="card-header">
-        
-        <div class="row ">
-            <div class="col-md-4">
-                <h4>{{ ucwords($user->name) }}</h4>
-            </div>
-            <div class="col-md-4" ">
-              
+<div class="row">
+    <div class="col">
+        <div class="card card-info">
+            <div class="card-header">User Info</div>
+            <div class="card-body">
+
+
+                {{-- <dl class="row">
+                    @foreach($user->toArray() as $key => $value)
+                        <dt class="col-sm-4 text-right text-uppercase">{{ str_replace("_", " " , $key) }}</dt>
+                        <dd class="col-sm-8 text-left">{{ $value }}</dd>
+                    @endforeach
+                </dl> --}}
+                <dl class="row">
+                <dt class="col-sm-4 text-right text-uppercase">user id</dt>
+                <dd class="col-sm-8 text-left">{{ $user->id }}</dd>
+
+                <dt class="col-sm-4 text-right text-uppercase">user email</dt>
+                <dd class="col-sm-8 text-left">{{ $user->email }}</dd>
+
+                <dt class="col-sm-4 text-right text-uppercase">phone</dt>
+                <dd class="col-sm-8 text-left">{{ $user->phone }}</dd>
+                </dl>
 
             </div>
-            <div class="col-md-4 ">
-                <div class="row float-right ">
-                <a 
-                    href="{{ route('users.edit', $user->id) }}"
-                    class="btn btn-success"  
-                    role="button">
-                    <i class="fas fa-edit"></i> {{ __('Update') }}
-                 </a>
-                    &nbsp;
-                <a  class="btn btn-danger"  
-                    role="button"
-                    onclick="event.preventDefault();
-                    document.getElementById('delete-form').submit();">
-                     <i class='fas fa-trash'></i>  {{ __('Delete') }}
-                 </a>
-                </div>
-        
-            </div>
+            <div class="card-footer text-muted ">Registered <span>{{ \Carbon\Carbon::parse($user->created_at)->diffForHumans() }}</span></div>
         </div>
-
-
     </div>
-    <!-- /.card-header -->
-
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered table-striped">
-                <tbody>
-                <tr>
-                  <th style="width:10%">First Name</th>
-                  <th>{{ $user->firstname }} </th>
-                </tr>
-
-                <tr>
-                    <th style="width:10%">Last Name</th>
-                    <th>{{ $user->firstname }} </th>
-                  </tr>
-                <tr>
-                    <th style="width:10%">Email</th>
-                    <th>{{ $user->email }} </th>
-                </tr>
-                <tr>
-                    <th style="width:10%">Joined</th>
-                    <th>{{ $user->created_at->diffForhumans() }} </th>
-                </tr>
-                <tr>
-                    <th style="width:10%">Roles</th>
-                    <th>
-                    @foreach( $user->getRoleNames() as $role ) 
-                        {{ ucfirst($role) }}
-                        @if(count($user->getRoleNames()) > 1 )
-                           ,
-                        @endif
-                    @endForeach
-                    </th>
-                </tr>
-                </tbody>
-            </table>
-          </div>
-        
-    </div>
-    <!-- /.card-body -->
-    
-
-
-
-
 </div>
-</form>    
+
+<div class="row">
+    <div class="col">
+        <div class="card card-info">
+            <div class="card-header">Company Info</div>
+            <div class="card-body">
+
+
+                {{-- <dl class="row">
+                    @foreach($user->company->toArray() as $key => $value)
+                        <dt class="col-sm-4 text-right text-uppercase">{{ str_replace("_", " " , $key) }}</dt>
+                        <dd class="col-sm-8 text-left">{!! $value === 1 ? '<i class="fa fa-check text-success" aria-hidden="true"></i>' : $value!!}</dd>
+                    @endforeach
+                </dl> --}}
+
+
+                <dl class="row">
+
+                        <dt class="col-sm-4 text-right text-uppercase">company id</dt>
+                        <dd class="col-sm-8 text-left">{{ $user->company->id }}</dd>
+
+                        <dt class="col-sm-4 text-right text-uppercase">Is Approved ?</dt>
+                        <dd class="col-sm-8 text-left">{{ $user->company->is_approved }}</dd>
+
+                        <dt class="col-sm-4 text-right text-uppercase">company name</dt>
+                        <dd class="col-sm-8 text-left">{{ $user->company->name }}</dd>
+
+                        <dt class="col-sm-4 text-right text-uppercase">company email</dt>
+                        <dd class="col-sm-8 text-left">{{ $user->company->email }}</dd>
+
+                        <dt class="col-sm-4 text-right text-uppercase">company phone</dt>
+                        <dd class="col-sm-8 text-left">{{ $user->company->phone }}</dd>
+
+                </dl>
+            </div>
+            <div class="card-footer text-muted">Submitted <span>{{ \Carbon\Carbon::parse($user->company->created_at)->diffForHumans() }}</div>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col">
+        <div class="card card-info">
+            <div class="card-header">Company Info</div>
+            <div class="card-body">
+
+
+             @foreach($user->proposals as $proposal)
+                {{$proposal->channel}}
+             @endforeach
+            </div>
+            <div class="card-footer text-muted">Submitted <span>{{ \Carbon\Carbon::parse($user->company->created_at)->diffForHumans() }}</div>
+        </div>
+    </div>
+</div>
+
 
 <form id="delete-form" action="{{ route('users.destroy', $user->id) }}" method="POST" class="d-none">
-    @csrf 
+    @csrf
     @method('DELETE')
 </form>
 @stop
