851ae2c2b5f0f37857341fe6b4c9ee2d1f27b94b
Azril Nazli Alias <azril.nazli@gmail.com>
2022-05-20 12:19:29 +0800

    Company - updaate edit

--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -47,6 +47,12 @@
             <dt class="col-sm-2">State</dt>
             <dd class="col-sm-9">{{ $company->states }}</dd>
 
+            <dt class="col-sm-2">User ID</dt>
+            <dd class="col-sm-9">{{ $company->user->id}}</dd>
+
+            <dt class="col-sm-2">User E-Mail</dt>
+            <dd class="col-sm-9">{{ $company->user->email}}</dd>
+
             <dt class="col-sm-2">Registered On</dt>
             <dd class="col-sm-9">{{ $company->user->created_at }} , {{ $company->user->created_at->diffForHumans() }}</dd>
 
