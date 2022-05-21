12e66c96c713ef6f8516eb863eb51efd5b6e8016
Azril Nazli Alias <sales@stream.my>
2022-05-21 20:12:47 +0800

    DASHBOARD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,121 +1,234 @@
 @extends('layouts.master')
 
 @section('name', 'Dashboard')
+
 @section('content')
 
+<!-- Horizontal Form -->
 <div class="row">
-    <div class="col-md-2">
-            <div class="info-box bg-secondary">
-                    <span class="info-box-icon"><i class="far fa-user"></i></span>
-                    <div class="info-box-content">
-                            <span class="info-box-text">New Register</span>
-                            <span class="info-box-number">{{ $total['registered'] }}</span>
+    <div class="col">
+        <div class="card card-info">
+            <div class="card-header">User </div>
+            <div class="card-body">
+                <!-- Registered -->
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Total</span>
+                                        <span class="info-box-number">{{ $user['total'] }}</span>
+                                </div>
+                        </div>
                     </div>
+                    <!-- ./ Registered -->
+                    <!-- Admin -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Admin</span>
+                                        <span class="info-box-number">{{ $user['admin'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Admin -->
+                    <!-- Subscriber -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Vendor</span>
+                                        <span class="info-box-number">{{ $user['vendor'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Subscriber -->
+                    <!-- Comments -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Comments</span>
+                                        <span class="info-box-number">{{ $comment['total'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Comments -->
+                </div> <!-- ./row -->
+            </div>
+            <div class="card-footer text-muted ">
 
             </div>
-    </div>
-
-    <div class="col-md-2">
-            <div class="info-box bg-info">
-                    <span class="info-box-icon"><i class="far fa-user"></i></span>
-                    <div class="info-box-content">
-                            <span class="info-box-text">Pending</span>
-                            <span class="info-box-number">{{ $total['pending'] }}</span>
-                    </div>
-
-            </div>
-    </div>
-    <div class="col-md-2">
-        <div class="info-box bg-success">
-                <span class="info-box-icon"><i class="far fa-user"></i></span>
-                <div class="info-box-content">
-                        <span class="info-box-text">Approved</span>
-                        <span class="info-box-number">{{ $total['approved'] }}</span>
-                </div>
-
-        </div>
-    </div>
-    <div class="col-md-2">
-        <div class="info-box bg-danger">
-                <span class="info-box-icon"><i class="far fa-user"></i></span>
-                <div class="info-box-content">
-                        <span class="info-box-text">Rejected</span>
-                        <span class="info-box-number">{{ $total['rejected'] }}</span>
-                </div>
-
-        </div>
-    </div>
-
-    <div class="col-md-2">
-        <div class="info-box bg-warning">
-                <span class="info-box-icon"><i class="far fa-user"></i></span>
-                <div class="info-box-content">
-                        <span class="info-box-text">Resubmit</span>
-                        <span class="info-box-number">{{ $total['resubmit'] }}</span>
-                </div>
-
         </div>
     </div>
 </div>
+<!-- ./Horizontal Form -->
 
-<div class="alert alert-light" role="alert">
-    <h4 class="alert-heading">TVCMS Admin</h4>
-    <p>This is TVCMS Administration System. Main navigation is using Side Menu on the left.</p>
-</div>
-
+<!-- Horizontal Form -->
 <div class="row">
-    <div class="col-sm-6 first-column">
-      <h5>Newly registered vendor <a href="{{ route('companies.is_new') }}" class="btn btn-sm btn-primary">VIEW</a></h5>
-        @foreach($users as $user)
-            <div class="alert alert-secondary" role="alert">
-                {{$user->email}} just signed up {{$user->created_at->diffForHumans()}}.
-            </div>
-        @endForeach
-    </div>
-    <div class="col-sm-6 second-column">
-        <h5>Request for Approval <a href="{{ route('companies.is_pending') }}" class="btn btn-sm btn-primary">VIEW</a></h5>  
-        @foreach($requested as $user)
-            <div class="alert alert-info" role="alert">
-                {{ $user->name ? $user->name : $user->email }} requested {{$user->updated_at->diffForHumans()}}.
-            </div>
-        @endForeach
-    </div> 
-  </div>
+    <div class="col">
+        <div class="card card-info">
+            <div class="card-header">Company </div>
+            <div class="card-body">
+                <!-- Total -->
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Total</span>
+                                        <span class="info-box-number">{{ $company['total'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./ Total -->
+                    <!-- Pending -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Pending</span>
+                                        <span class="info-box-number">{{ $company['pending'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Pending -->
+                    <!-- Rejected -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Rejected</span>
+                                        <span class="info-box-number">{{ $company['rejected'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Rejected -->
 
-  <div class="row mt-5">
-    <div class="col-sm-6 first-column">
-      <h5>Approved <a href="{{ route('companies.is_approved') }}" class="btn btn-sm btn-primary">VIEW</a></h5>
-        @foreach($approved as $user)
-            <div class="alert alert-success" role="alert">
-                {{ $user->name ? $user->name : $user->email }} was approved {{$user->updated_at->diffForHumans()}}.
-            </div>
-        @endForeach
-    </div>
-    <div class="col-sm-6 second-column">
-        <h5>Rejected <a href="{{ route('companies.is_rejected') }}" class="btn btn-sm btn-primary">VIEW</a></h5>
-        @foreach($rejected as $user)
-            <div class="alert alert-danger" role="alert">
-                {{ $user->name ? $user->name : $user->email }} request was rejected {{$user->updated_at->diffForHumans()}}.
-            </div>
-        @endForeach
-    </div> 
-  </div>
+                    <!-- Approved -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Approved</span>
+                                        <span class="info-box-number">{{ $company['approved'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Approved -->
 
-  
-  <div class="row mt-5">
-    <div class="col-sm-6 first-column">
-      <h5>Resubmit  <a href="{{ route('companies.is_resubmit') }}" class="btn btn-sm btn-primary">VIEW</a></h5>
-        @foreach($resubmit as $user)
-            <div class="alert alert-warning" role="alert">
-                {{ $user->name ? $user->name : $user->email }} was resubmit {{$user->updated_at->diffForHumans()}}.
+                    <!-- Resubmit -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Resubmit</span>
+                                        <span class="info-box-number">{{ $company['resubmit'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Resubmit -->
+                </div> <!-- ./row -->
             </div>
-        @endForeach
+            <div class="card-footer text-muted ">
+                <a href="{{ route('companies.is_new') }}" class="btn btn-sm btn-primary">REGISTRATION</a>
+                <a href="{{ route('companies.is_pending' ) }}" class="btn btn-sm btn-primary">PENDING</a>
+                <a href="{{ route('companies.is_approved') }}" class="btn btn-sm btn-primary">APPROVED</a>
+                <a href="{{ route('companies.is_rejected') }}" class="btn btn-sm btn-primary">REJECTED</a>
+                <a href="{{ route('companies.is_resubmit') }}" class="btn btn-sm btn-primary">RESUBMIT</a></h5>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-6 second-column">
+</div>
+<!-- ./Horizontal Form -->
 
-    </div> 
-  </div>
+
+<!-- Horizontal Form -->
+<div class="row">
+    <div class="col">
+        <div class="card card-info">
+            <div class="card-header">Proposal </div>
+            <div class="card-body">
+
+                <div class="row">
+                    <!-- Registered -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Total</span>
+                                        <span class="info-box-number">{{ $proposal['total'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./ Registered -->
+                    <!-- SAMBUNG SIRI -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">SAMBUNG SIRI</span>
+                                        <span class="info-box-number">{{ $proposal['sambung_siri'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./ SAMBUNG SIRI -->
+                    <!-- SWASTA -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">SWASTA</span>
+                                        <span class="info-box-number">{{ $proposal['swasta'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./ SWASTA -->
+                    <!-- PDF Only -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">PDF only</span>
+                                        <span class="info-box-number">{{ $proposal['pdf_only'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./PDF Only -->
+                    <!-- Video Only -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">Video only</span>
+                                        <span class="info-box-number">{{ $proposal['video_only'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Video Only -->
+                    <!-- Both Only -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">PDF & Video</span>
+                                        <span class="info-box-number">{{ $proposal['both'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./Both Only -->
+                </div> <!-- ./row -->
+            </div>
+            <div class="card-footer text-muted ">
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ./Horizontal Form -->
 @endsection
+
 
 
 
