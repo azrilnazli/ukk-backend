b15355aa2727fda1b4ada93f47482393460fe47d
Azril Nazli Alias <sales@stream.my>
2022-05-23 23:08:40 +0800

    PROPOSAL - search token page 2

--- app/Services/TenderSubmissionService.php
+++ app/Services/TenderSubmissionService.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 namespace App\Services;
 
 use App\Models\TenderSubmission;
 use Auth;
 
 class TenderSubmissionService {
-    
+
 
     // contstructor
     public function __construct(){
-       
+
     }
 
     // public function paginate($items = 50){
@@ -20,7 +20,7 @@ class TenderSubmissionService {
     {
         return TenderSubmission::query()
             ->sortable()
-            ->whereHas('user.company', fn($query) => 
+            ->whereHas('user.company', fn($query) =>
                 $query->where('is_approved', true)
                 )
             ->orderBy('id','desc')
@@ -30,37 +30,42 @@ class TenderSubmissionService {
 
     public function search($request)
     {
-        $q = $request->input('query'); 
-               
+        $q = $request->input('query');
+        $t = $request->input('_token');
+
         // return Tender::where([['title', 'like', "{$query}%"]])
         //                 ->paginate(10)->setPath('tenders');
         $tenders = TenderSubmission::query()
                         ->sortable()
-                        // ->whereHas('user.company', fn($query) => 
+                        // ->whereHas('user.company', fn($query) =>
                         //     $query->where('is_approved', true)
                         // )
-                        ->orWhereHas('user.company', fn($query) =>   
+                        ->orWhereHas('user.company', fn($query) =>
                             $query->where('name', 'LIKE', '%' . $q . '%')
                             ->orWhere('email', 'LIKE', '%' . $q . '%')
-                            ->orWhere('id', 'LIKE', '%' . $q . '%') 
-                            ->orWhere('phone', 'LIKE', '%' . $q . '%')   
- 
+                            ->orWhere('id', 'LIKE', '%' . $q . '%')
+                            ->orWhere('phone', 'LIKE', '%' . $q . '%')
+
                         )
-                        ->orWhereHas('tender', fn($query) =>   
+                        ->orWhereHas('tender', fn($query) =>
                             $query->where('tender_category', 'LIKE', '%' . $q . '%')
                             ->orWhere('type', 'LIKE', '%' . $q . '%')
                             ->orWhere('duration', 'LIKE', '%' . $q . '%')
-                            ->orWhere('channel', 'LIKE', '%' . $q . '%')   
-                            ->orWhere('programme_code', 'LIKE', '%' . $q . '%') 
+                            ->orWhere('channel', 'LIKE', '%' . $q . '%')
+                            ->orWhere('programme_code', 'LIKE', '%' . $q . '%')
                         )
-                
+
                         ->paginate(50)
                         ->setPath(route('tender_submissions.index'));
-                  
-                        $tenders->appends(['search' => $q]);
+
+                        $tenders->appends([
+                            '_token' => $t,
+                            'search' => $q
+                            ]
+                        );
 
         return $tenders;
-        
+
     }
 
     public function create($request){
@@ -80,7 +85,7 @@ class TenderSubmissionService {
     public function find($id){
         return TenderSubmission::find($id);
     }
-    
+
     public function update($request, $id){
         // return Tender::where('id',$id)->update([
         //     'channel' => $request['channel'],
@@ -89,7 +94,7 @@ class TenderSubmissionService {
         //     'tender_category' => $request['tender_category'],
         //     'title' => $request['title'],
         //     'description' => $request['description'],
-        // ]); 
+        // ]);
 
         return TenderSubmission::where('id',$id)->update($request->except(['_token','_method']));
     }
@@ -97,5 +102,5 @@ class TenderSubmissionService {
     public function destroy($id){
         return TenderSubmission::where('id',$id)->delete();
     }
-        
-} 
+
+}
