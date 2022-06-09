dc36df713066b88247b638ed39b623b11eca3d60
Azril Nazli Alias <sales@stream.my>
2022-06-09 22:21:18 +0800

    JSPD ADMIN - index

---
+++ app/Http/Controllers/JSPD/JspdAdminController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers\JSPD;
+
+use App\Http\Controllers\Controller;
+use App\Models\TenderSubmission;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Models\Scoring;
+use Auth;
+use App\Services\JspdAdminService;
+
+class JspdAdminController extends Controller
+{
+    function __construct()
+    {
+        $this->middleware( 'permission:jspd-admin-list',     ['only' => ['dashboard','index','show','search']] );
+        $this->middleware( 'permission:jspd-admin-create',   ['only' => ['create','store']] );
+        $this->middleware( 'permission:jspd-admin-edit',     ['only' => ['edit','update']] );
+        $this->middleware( 'permission:jspd-admin-delete',   ['only' => ['delete']] );
+
+        $this->service = new JspdAdminService;
+    }
+
+    public function dashboard(){}
+    public function index(){
+        // list all tender_submissions
+        // count assigned signers
+        // count assigned urusetias
+        // searchable by company name & id
+        $proposals = $this->service->paginate(50);
+        return view('JSPD.admins.index', compact('proposals'));
+    }
+    public function show(){}
+    public function search(){}
+    public function create(){}
+    public function store(){}
+    public function edit(){}
+    public function update(){}
+    public function delete(){}
+}
---
+++ app/Services/JspdAdminService.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Services;
+
+use App\Models\TenderSubmission;
+use Auth;
+
+class JspdAdminService {
+
+
+    // contstructor
+    public function __construct(){
+
+    }
+
+
+
+    public function paginate($item = 50)
+    {
+        return TenderSubmission::query()
+            ->sortable()
+            ->whereHas('user.company', fn($query) =>
+                $query->where('is_approved', true)
+                )
+            ->orderBy('id','desc')
+            ->paginate($item)
+            ->setPath(route('jspd-admins.index'));
+    }
+
+    public function search($request)
+    {
+        $q = $request->input('query');
+        $tenders = TenderSubmission::query()
+
+                        ->orWhereHas('user.company', fn($query) =>
+                            $query->where('name', 'LIKE', '%' . $q . '%')
+                            ->orWhere('email', 'LIKE', '%' . $q . '%')
+                            ->orWhere('id', 'LIKE', '%' . $q . '%')
+                            ->orWhere('phone', 'LIKE', '%' . $q . '%')
+
+                        )
+                        ->orWhereHas('tender', fn($query) =>
+                            $query->where('tender_category', 'LIKE', '%' . $q . '%')
+                            ->orWhere('type', 'LIKE', '%' . $q . '%')
+                            ->orWhere('duration', 'LIKE', '%' . $q . '%')
+                            ->orWhere('channel', 'LIKE', '%' . $q . '%')
+                            ->orWhere('programme_code', 'LIKE', '%' . $q . '%')
+                        )
+
+                        ->paginate(50)
+                        ->setPath(route('jspd-admins.search'));
+
+                        $tenders->appends([
+                            'query' => $q
+                            ]);
+        return $tenders;
+
+    }
+
+    public function create($request){
+        return TenderSubmission::create($request->except(['_token','_method']));
+    }
+
+    public function find($id){
+        return TenderSubmission::find($id);
+    }
+
+    public function update($request, $id){
+        return TenderSubmission::where('id',$id)->update($request->except(['_token','_method']));
+    }
+
+    public function destroy($id){
+        return TenderSubmission::where('id',$id)->delete();
+    }
+
+}
