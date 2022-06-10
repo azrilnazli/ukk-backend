9cd1a7c7a869cef9d3c2e6959807ae5e3f4a883a
Azril Nazli Alias <azril.nazli@gmail.com>
2022-06-10 11:28:44 +0800

    SCORE - adjust

--- resources/views/JSPD/scorings/contents.blade.php
+++ resources/views/JSPD/scorings/contents.blade.php
@@ -1,8 +1,8 @@
 <nav>
-    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <div class="nav nav-tabs " id="nav-tab" role="tablist">
       {{-- <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab">SUMMARY</a> --}}
       @foreach($scorings as $key => $score )
-        <a class="nav-item nav-link text-uppercase" id="nav-scoring-tab" data-toggle="tab" href="#score_{{ $score->id }}" role="tab">{{ optional($score)->user->name }}</a>
+        <a class="nav-item nav-link text-uppercase @if($key ==0) show active @endif" id="nav-scoring-tab" data-toggle="tab" href="#score_{{ $score->id }}" role="tab">{{ optional($score)->user->name }}</a>
       @endforeach
 
     </div>
@@ -12,7 +12,7 @@
         @include('JSPD.scorings.summary')
     </div> --}}
     @foreach($scorings as $key => $score )
-      <div class="tab-pane fade p-2" id="score_{{ $score->id }}" role="tabpanel">
+      <div  class="tab-pane fade p-2 @if($key ==0) show active @endif" id="score_{{ $score->id }}" role="tabpanel">
         @include('JSPD.scorings.form_verify', ['data' => $score])
       </div>
     @endforeach
