faade7dd5aeb569425912150f10d6d4968efffd5
Azril Nazli Alias <azril.nazli@gmail.com>
2022-06-11 18:44:47 +0800

    HOME - saringan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -293,6 +293,19 @@
                     </div>
                     <!-- ./ Registered -->
 
+                    
+                    <!-- Registered -->
+                    <div class="col-md-2">
+                        <div class="info-box bg-secondary">
+                                {{-- <span class="info-box-icon"><i class="far fa-user"></i></span> --}}
+                                <div class="info-box-content">
+                                        <span class="info-box-text">3 PENANDA 2 URUSETIA</span>
+                                        <span class="info-box-number">{{ $proposal['signers'] }}</span>
+                                </div>
+                        </div>
+                    </div>
+                    <!-- ./ Registered -->
+
                     <!-- Registered -->
                     <div class="col-md-2">
                         <div class="info-box bg-secondary">
