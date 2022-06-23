4954267cbdbb30ee78fc7ee690a18109b91fe745
Azril Nazli Alias <azril.nazli@gmail.com>
2022-06-23 13:28:03 +0800

    HOME - updated

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -69,17 +69,7 @@ class HomeController extends Controller
             return redirect()->to(route('scorings.dashboard'));
         }
 
-
-        $resubmit = Company::query()
-        ->orderBy('updated_at','desc')
-        ->where('is_completed', true)
-        ->where('is_rejected', true)
-        ->limit(5)
-        ->get();
-
-
-
-        // user related
+        $company['total'] = Company::query()->count();
         $user['total'] = User::query()->count();
         $user['admin'] = User::role('admin')->count(); ;
         $user['vendor'] = User::role('subscriber')->count(); ;
@@ -94,111 +84,9 @@ class HomeController extends Controller
         $video['failed'] = Video::query()->where('duration','=', 0)->count();
 
 
-        // proposal related
-        $proposal['total'] = TenderSubmission::query()
-                                // ->whereHas('user.company', fn($query) =>
-                                //         $query->where('is_approved', true)
-                                //     )
-                                ->has('user.approved_company')
-                                ->count();
-
-        $proposal['assigned'] = TenderSubmission::query()
-                            ->has('user.approved_company')
-                            ->where('added_by','!=',0)
-                            ->count();
-
-        $proposal['signed'] = TenderSubmission::query()
-                            ->has('scorings','=', 3)
-                            ->count();
-
-        $proposal['verified'] = TenderSubmission::query()
-                            ->has('verifications','=', 2)
-                            ->count();
-
-        $proposal['approved'] = TenderSubmission::query()
-                            ->has('approval','=', 1)
-                            ->count();
-
-        $proposal['signers'] = TenderSubmission::query()
-                            ->has('signers','=', 3)
-                            ->has('urusetias','=', 2)
-                            ->count();
-
-        $proposal['success'] = TenderSubmission::query()
-                            ->whereHas('user.company', fn($query) =>
-                              $query->where('is_approved', true)
-                              )
-                            ->has('scorings','=', 3)
-                            ->has('verifications','=', 2)
-                            ->has('approved','>=', 2)
-                            ->count();
-
-        $proposal['failed'] = TenderSubmission::query()
-                            ->whereHas('user.company', fn($query) =>
-                              $query->where('is_approved', true)
-                              )
-                            ->has('scorings','=', 3)
-                            ->has('verifications','=', 2)
-                            ->has('failed','>=', 2)
-                            ->count();
-
-        $proposal['pending'] = TenderSubmission::query()
-                            ->whereHas('user.company', fn($query) =>
-                              $query->where('is_approved', true)
-                              )
-                            ->has('scorings','!=', 3)
-                            ->has('verifications','!=', 2)
-                            ->count();
 
 
 
-
-        //$proposal['pdf_only'] = TenderSubmission::query()->where('is_pdf_cert_uploaded','=', true)->count();
-        //$proposal['video_only'] = TenderSubmission::query()->where('video_id','!=', 0)->count();
-        $proposal['both'] = TenderSubmission::query()
-                            ->whereHas('user.company', fn($query) =>
-                                $query->where('is_approved', true)
-                             )
-                             ->whereHas('video', fn($query) =>
-                             $query->where('is_ready', true)
-                         )
-                            ->where('is_pdf_cert_uploaded','=', 1)
-                            ->count();
-
-        $proposal['pdf_only'] = TenderSubmission::query()
-                                ->whereHas('user.company', fn($query) =>
-                                    $query->where('is_approved', true)
-                                )
-                                ->where('is_pdf_cert_uploaded','=', true)
-                                ->count();
-
-        $proposal['video_only'] = TenderSubmission::query()
-                                ->whereHas('video', fn($query) =>
-                                    $query->where('is_ready', true)
-                                )
-                                ->whereHas('user.company', fn($query) =>
-                                    $query->where('is_approved', true)
-                                )
-                                ->where('is_pdf_cert_uploaded','=', false)
-                                ->count();
-
-        $proposal['sambung_siri'] = TenderSubmission::query()
-                                ->whereHas('tender', fn($query) =>
-                                    $query->where('type', 'SAMBUNG SIRI')
-                                )
-                                ->whereHas('user.company', fn($query) =>
-                                    $query->where('is_approved', true)
-                                )
-                                ->count();
-
-        $proposal['swasta'] = TenderSubmission::query()
-                                ->whereHas('tender', fn($query) =>
-                                    $query->where('type', 'SWASTA')
-                                )
-                                ->whereHas('user.company', fn($query) =>
-                                    $query->where('is_approved', true)
-                                )
-                                ->count();
 
         // state related
         $states = Company::distinct()
@@ -218,30 +106,6 @@ class HomeController extends Controller
         ->where('states', 'LIKE', '%selangor%')
         ->count();
 
-        // company related
-        $company['total'] = Company::query()->count();
-
-        $company['pending'] = Company::query()
-        ->where('is_completed', true)
-        ->where('is_rejected', false)
-        ->where('is_approved', false)
-        ->count();
-
-        $company['rejected'] = Company::query()
-        ->orderBy('updated_at','desc')
-        ->where('is_completed', false)
-        ->where('is_rejected', true)
-        ->count();
-
-        $company['approved'] = Company::query()
-        ->where('is_completed', true)
-        ->where('is_approved', true)
-        ->count();
-
-        $company['resubmit'] = Company::query()
-        ->where('is_completed', true)
-        ->where('is_rejected', true)
-        ->count();
 
         //dd($total);
 
@@ -272,45 +136,17 @@ class HomeController extends Controller
 
        //dd($video);
 
-       // tender related
-       $tenders['sambung_siri'] = Tender::query()
-       ->distinct()
-       ->where('type','SAMBUNG SIRI')
-       ->select('id','programme_code')
-       ->orderBy('programme_code', 'ASC')
-       ->get(['programme_code'])
-       ->map( function($val, $key)  {
-
-           $val['count'] = TenderSubmission::query()
-           ->where('tender_id', $val->id)
-           ->count();
-           return $val;
-       });
-
-       $tenders['swasta'] = Tender::query()
-       ->distinct()
-       ->where('type','SWASTA')
-       ->select('id','programme_code')
-       ->orderBy('programme_code', 'ASC')
-       ->get(['programme_code'])
-       ->map( function($val, $key)  {
-
-           $val['count'] = TenderSubmission::query()
-           ->where('tender_id', $val->id)
-           ->count();
-           return $val;
-       });
 
 
         return view('home')->with(compact(
-            'user',
+
             'company',
-            'proposal',
+
             'comment',
-            'resubmit',
+
             'video',
             'states',
-            'tenders'
+
         ));
     }
 
