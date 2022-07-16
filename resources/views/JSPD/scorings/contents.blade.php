537ff4b0ac4b4f335472bad3b765a95cfdd1b3ed
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-16 15:01:18 +0800

    remove urusetia is required

--- resources/views/JSPD/scorings/contents.blade.php
+++ resources/views/JSPD/scorings/contents.blade.php
@@ -3,7 +3,7 @@
     <div class="nav nav-tabs " id="nav-tab" role="tablist">
       {{-- <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab">SUMMARY</a> --}}
     @foreach($scorings as $key => $score )
-        @if(!empty($scorings$score->user))
+        @if(!empty($score->user))
             <a class="nav-item nav-link text-uppercase @if($key ==0) show active @endif" id="nav-scoring-tab" data-toggle="tab" href="#score_{{ $score->id }}" role="tab">{{ $score->user ? $score->user->name : null }}</a>
         @else
             <a class="nav-item nav-link text-uppercase @if($key ==0) show active @endif" id="nav-scoring-tab" data-toggle="tab" href="#score_{{ $score->id }}" role="tab">USER NOT EXIST</a>
@@ -18,7 +18,7 @@
         @include('JSPD.scorings.summary')
     </div> --}}
     @foreach($scorings as $key => $score )
-        @if(!empty($scorings$score->user))
+        @if(!empty($score->user))
         <div  class="tab-pane fade p-2 @if($key ==0) show active @endif" id="score_{{ $score->id }}" role="tabpanel">
             @include('JSPD.scorings.form_verify', ['data' => $score])
         </div>
