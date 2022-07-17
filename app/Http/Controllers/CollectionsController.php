d41f05b3845e467c671806ae70bc594a06492eda
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-17 22:48:31 +0800

    show:method proposal_new

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -1415,9 +1415,9 @@ class CollectionsController extends Controller
 
         function proposal_new(){
             // date start = 2022-06-21
-            $result = tenderSubmission::query()
+            $result = \App\Models\CompanyApproval::query()
             ->whereBetween('created_at', ['2022-06-21', now() ])
-            ->whereHas('tender.tender_detail', fn($query) =>
+            ->whereHas('tender_detail', fn($query) =>
                 $query->whereIn('id', [3,4])
             )
             ->get()
