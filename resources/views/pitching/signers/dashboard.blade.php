9cea66b706855308cf46cd0bba30d2eafd97e855
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-17 21:18:30 +0800

    DASHBOARD - PDF

--- resources/views/pitching/signers/dashboard.blade.php
+++ resources/views/pitching/signers/dashboard.blade.php
@@ -13,9 +13,5 @@
 @stop
 
 @section('content')
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet justo donec enim diam. Quisque id diam vel quam elementum pulvinar. Purus non enim praesent elementum facilisis. Vitae tortor condimentum lacinia quis vel eros donec. Suspendisse in est ante in nibh mauris cursus mattis molestie. Volutpat blandit aliquam etiam erat velit. Quis ipsum suspendisse ultrices gravida dictum fusce ut. Id semper risus in hendrerit gravida rutrum. Vestibulum mattis ullamcorper velit sed ullamcorper morbi tincidunt. Nulla facilisi cras fermentum odio eu feugiat pretium nibh ipsum. A scelerisque purus semper eget.
-
-Sed elementum tempus egestas sed. Volutpat odio facilisis mauris sit amet massa. Cras semper auctor neque vitae tempus quam pellentesque nec. Nulla facilisi nullam vehicula ipsum a arcu cursus. Rhoncus urna neque viverra justo nec. Senectus et netus et malesuada fames. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Massa massa ultricies mi quis hendrerit. Dignissim enim sit amet venenatis urna cursus eget nunc scelerisque. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Lacus viverra vitae congue eu. In nibh mauris cursus mattis molestie a iaculis.
-</pre>
+    @include('pitching.partials.dashboard')
 @stop
