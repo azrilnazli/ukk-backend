f3e01766438628b2d55b331731f7205406dc273e
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-20 09:52:31 +0800

    Fix PitchingSigner detail

--- app/Services/Pitching/SignerService.php
+++ app/Services/Pitching/SignerService.php
@@ -98,7 +98,7 @@ class SignerService {
                         //     $query->where('is_approved', true)
                         // )
                         // ->whereIn('tender_detail_id',[1,2])
-                        ->orWhereHas('tender.tender_detail', fn($query) =>
+                        ->has('tender.tender_detail', fn($query) =>
                             $query->whereIn('id', [1,2])
                         )
 
