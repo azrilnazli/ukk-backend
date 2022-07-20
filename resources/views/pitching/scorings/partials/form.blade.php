9f22b42dc6a950d87de390b34e9dbe94a463fd3f
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-20 09:26:19 +0800

    PitchingScoring guideline table

--- resources/views/pitching/scorings/partials/form.blade.php
+++ resources/views/pitching/scorings/partials/form.blade.php
@@ -1,8 +1,32 @@
 @include('pitching.scorings.partials.form_idea', array('pitchingScoring' => $tenderSubmission->pitching_scoring ))
 @include('pitching.scorings.partials.form_kandungan', array('pitchingScoring' => $tenderSubmission->pitching_scoring ))
 @include('pitching.scorings.partials.form_comment', array('pitchingScoring' => $tenderSubmission->pitching_scoring ))
-<div class="col-6"><hr /></div>
+
+<table class="table table-bordered col-8">
+    <tr>
+        <td class="bg-danger text-center">GAGAL</td>
+        <td class="bg-warning text-center">BIASA</td>
+        <td style="background-color:yellow" class="text-center">SEDERHANA BAIK</td>
+        <td class="bg-success text-center">BAIK</td>
+        <td class="bg-success text-center">SANGAT BAIK</td>
+    </tr>
+    <tr>
+        <td class="text-center">0-79</td>
+        <td class="text-center">80-85</td>
+        <td class="text-center">86-90</td>
+        <td class="text-center">91-95</td>
+        <td class="text-center">96-100</td>
+    </tr>
+
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td class="text-center" colspan=2>DISYORKAN</td>
+    </tr>
+</table>
+
 @include('pitching.scorings.partials.form_scores', array('pitchingScoring' => $tenderSubmission->pitching_scoring ))
-<div class="col-6"><hr /></div>
+
 @include('pitching.scorings.partials.form_comply', array('pitchingScoring' => $tenderSubmission->pitching_scoring ))
 
