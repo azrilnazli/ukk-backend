cd17e3132b0422fb080af4e80cd4ee84a62ba700
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-26 19:38:50 +0800

    remove realtime score

--- resources/views/screening/scorings/partials/form.blade.php
+++ resources/views/screening/scorings/partials/form.blade.php
@@ -16,30 +16,4 @@
 @include('screening.scorings.partials.score_guide')
 @include('screening.scorings.partials.form_comply', array('screeningScoring' => $tenderSubmission->screening_scoring ))
 
-<script>
-$(document).ready(function(){
-
-    console.log('realtime scoring')
-
-    var forms = ['criteria','storyline','creativity','technical','acting','value_added'];
-
-    var final = [];
-
-    $.each( forms, function( index, value ) {
-        //alert( index + ": " + value );
-        $("#form_" + value).change(function(){
-            var score = parseInt(this.value); ;
-
-            final[index] = score;
-            const total = final.reduce((partialSum, a) => partialSum + a, 0);
-            //const final_score = Math.round((total/60)*100);
-            console.log(total);
-            $("#result").html(total);
-
-        });
-    });
-
-
-});
-</script>
 
--- resources/views/screening/scorings/partials/form_scores.blade.php
+++ resources/views/screening/scorings/partials/form_scores.blade.php
@@ -20,3 +20,31 @@
 
     @endif
 </div>
+
+
+<script>
+    $(document).ready(function(){
+
+        console.log('realtime scoring')
+
+        var forms = ['criteria','storyline','creativity','technical','acting','value_added'];
+
+        var final = [];
+
+        $.each( forms, function( index, value ) {
+            //alert( index + ": " + value );
+            $("#form_" + value).change(function(){
+                var score = parseInt(this.value); ;
+
+                final[index] = score;
+                const total = final.reduce((partialSum, a) => partialSum + a, 0);
+                //const final_score = Math.round((total/60)*100);
+                console.log(total);
+                $("#result").html(total);
+
+            });
+        });
+
+
+    });
+    </script>
