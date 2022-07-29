aea42e91c14b414a13d5c2e37762622cf7f94476
Azril Nazli Alias <azril.nazli@gmail.com>
2022-07-29 12:47:45 +0800

    PDF Direct Link in ScreeningHeader

--- resources/views/screening/partials/company.blade.php
+++ resources/views/screening/partials/company.blade.php
@@ -376,9 +376,9 @@
             </div>
         </div>
     </div>
-    <pre>
-    <a href="/storage/companies/{{$company->id}}/{{$document}}_cert.pdf">{{$document}}_cert.pdf</a>
-    </pre>
+
+    <a target="_blank" href="/storage/companies/{{$company->id}}/{{$document}}_cert.pdf">{{$document}}_cert.pdf</a><br />
+
     @endforeach
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfobject/2.2.7/pdfobject.min.js" integrity="sha512-g16L6hyoieygYYZrtuzScNFXrrbJo/lj9+1AYsw+0CYYYZ6lx5J3x9Yyzsm+D37/7jMIGh0fDqdvyYkNWbuYuA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
